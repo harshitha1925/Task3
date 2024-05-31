0fdde9a86f7ee1fca6d0c60875b1dfd9308537f2
harshitha1925 <169145359+harshitha1925@users.noreply.github.com>
2024-05-31 20:42:41 +0530

    Task3.py

--- Task3.py
+++ Task3.py
@@ -19,20 +19,20 @@ def generate_password(length):
         any(c in punctuation for c in password)) :
         return password
 
-    def get_password_length():
-        while True :
-            try:                                
-                length = int(input("Enter the desired length for the password (minimum 8 characters):"))
-                if length < 8:
-                    print ("Password length should be at least 8 characters.")
-                else :
-                    return length
-            except ValueError:
-                print ("Invalid input. Please Enter a numeric value.")
+def get_password_length():
+    while True :
+        try:                                
+            length = int(input("Enter the desired length for the password (minimum 8 characters):"))
+            if length < 8:
+                print ("Password length should be at least 8 characters.")
+            else :
+                return length
+        except ValueError:
+            print ("Invalid input. Please Enter a numeric value.")
 
 def main():
     print ("Password Generator")
-    length = get_password_length ()
+    length = get_password_length()
     password = generate_password(length)
     print (f"Generated Password: {password}")
 
